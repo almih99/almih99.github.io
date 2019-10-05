a0c1e2846bc140e33e5864d674222e19fcc824d7
almih99 <almih99@mail.ru>
2019-10-05 09:33:49 +0300

    ajusting line indents

--- articles/life/main.js
+++ articles/life/main.js
@@ -1,22 +1,23 @@
 "use strict";
 
 (function () {
-  let position=0;
-  function rotate() {
-    let items=Array.from(
-        document.querySelectorAll("#day li")
-    );
-    for(let i=0; i<items.length; i++) {
-      if(position == i) {
-        items[i].classList.add("now");
-      } else {
-        items[i].classList.remove("now")
-      }
+    let position=0;
+    function rotate() {
+        let items=Array.from(
+            document.querySelectorAll("#day li")
+        );
+        for(let i=0; i<items.length; i++) {
+            if(position == i) {
+                items[i].classList.add("now");
+            } else {
+                items[i].classList.remove("now")
+            }
+        }
+        position=(position+1) % items.length;
     }
-    position=(position+1) % items.length;
-  }
-  // born
-  setInterval(rotate, 1000);
+    // born
+    setInterval(rotate, 1000);
 })();
+
 
 
